2e11df2548ff9345c5acef870a8cb871336c51f5
sham97-03 <shamjamous7@example.com>
2025-07-21 23:27:45 +0300

    same

--- app/Services/SuperAdmin/DoctorApprovalService.php
+++ app/Services/SuperAdmin/DoctorApprovalService.php
@@ -17,10 +17,32 @@ class DoctorApprovalService
     }
 
     public function listPending()
-    {
-        $doctors = $this->doctorRepo->getPendingProfiles();
-        return $this->unifiedResponse(true, 'Pending doctors fetched', $doctors);
-    }
+{
+    $doctors = $this->doctorRepo->getPendingProfiles();
+
+    $filtered = $doctors->map(function ($doctor) {
+        return [
+            'doctor_profile' => [
+                'id' => $doctor->id,
+                'about_me' => $doctor->about_me,
+                'years_of_experience' => $doctor->years_of_experience,
+                'specialty_id' => $doctor->specialty_id,
+                'certificate' => $doctor->certificate,
+                'status' => $doctor->status,
+            ],
+            'user' => [
+                'full_name' => $doctor->user->full_name,
+                'profile_photo' => $doctor->user->profile_photo,
+                'birthdate' => $doctor->user->birthdate,
+                'gender' => $doctor->user->gender,
+                'address' => $doctor->user->address,
+            ]
+        ];
+    });
+
+    return $this->unifiedResponse(true, 'Pending doctors fetched', $filtered);
+}
+
 
     public function approve($id)
     {
