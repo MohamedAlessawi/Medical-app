379bc2e43a8f41b516a0646f1d2afa7533c6171b
sham97-03 <shamjamous7@example.com>
2025-08-03 22:40:40 +0300

    changess

--- database/migrations/2025_01_25_154521_create_doctors_table.php
+++ database/migrations/2025_01_25_154521_create_doctors_table.php
@@ -6,25 +6,18 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
     {
-        Schema::table('doctors', function (Blueprint $table) {
+        Schema::create('doctors', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
             $table->foreignId('center_id')->constrained('centers')->onDelete('cascade');
-
+            $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
-        Schema::table('doctors', function (Blueprint $table) {
-            $table->dropForeign(['center_id']);
-            $table->dropColumn('center_id');
-        });
+        Schema::dropIfExists('doctors');
     }
 };
